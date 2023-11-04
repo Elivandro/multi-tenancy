92acd233a23a845ae7096b17520f83df328ebb48
Elivandro <Elivandro@hotmail.com.br>
2023-11-04 14:37:41 -0400

    layout adjustments and some imports

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -9,8 +9,11 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/css/main.d7f96858.css" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Scrips -->
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
 <body class="antialiased">
 
